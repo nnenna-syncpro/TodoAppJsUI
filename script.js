bf21be29cdeb78b98e5f6df1044c83fe0037193f
esot0001 <esot0001@algonquinlive.com>
2024-09-20 02:20:44 -0400

    feat: toggles home screen and todo list screen visible

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const wrapperEl = document.querySelector(".wrapper");
 const menuBtnEl = document.querySelector(".menu-btn");
+const backBtnEl = document.querySelector(".back-btn");
 
 const toggleScreen = () => {
   wrapperEl.classList.toggle("show-list");
 };
 menuBtnEl.addEventListener("click", toggleScreen);
+backBtnEl.addEventListener("click", toggleScreen);
