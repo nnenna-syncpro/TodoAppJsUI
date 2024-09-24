ee89ac5f054f4c5ea4f5b3bd2f25bae2da01e14a
esot0001 <esot0001@algonquinlive.com>
2024-09-24 16:53:40 -0400

    feat: displays card categories

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { default as categories } from "./data/categories.json" with { type: "json" };
+
 const wrapperEl = document.querySelector(".wrapper");
 const menuBtnEl = document.querySelector(".menu-btn");
 const backBtnEl = document.querySelector(".back-btn");
@@ -20,3 +22,30 @@ const toggleAddTaskForm = () => {
 
 addTaskBtnEl.addEventListener("click", toggleAddTaskForm);
 blackBackdropEl.addEventListener("click", toggleAddTaskForm);
+
+const categoriesContainerEl = document.querySelector(".categories");
+const renderCategories = () => {
+  categoriesContainerEl.innerHTML = "";
+  categories.forEach((category) => {
+    //render category cards
+    const categoryDiv = document.createElement("div");
+    categoryDiv.classList.add("category");
+    categoryDiv.innerHTML = `
+    <div class="category-details">
+      <img src="${category.img}" alt="${category.title} icon" />
+      <div class="content">
+        <h1>${category.title}</h1>
+        <p>0 Tasks</p>
+      </div>
+    </div>
+    <div class="category-options">
+      <div class="toggle-btn">
+        <img src="./assets/images/toggle-btn.svg" alt="" />
+      </div>
+    </div>
+    `;
+    categoriesContainerEl.appendChild(categoryDiv);
+  });
+};
+
+renderCategories();
