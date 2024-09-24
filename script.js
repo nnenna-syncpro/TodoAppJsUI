11f920737429d57ba299c8d34ce52392b3f4b1eb
esot0001 <esot0001@algonquinlive.com>
2024-09-24 09:40:00 -0400

    feat: toggles add task form visible on button click

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ const toggleScreen = () => {
 };
 menuBtnEl.addEventListener("click", toggleScreen);
 backBtnEl.addEventListener("click", toggleScreen);
+
+const addTaskBtnEl = document.querySelector(".add-task-btn");
+const addTaskFormEl = document.querySelector(".add-task-form");
+const blackBackdropEl = document.querySelector(".black-backdrop");
+
+const toggleAddTaskForm = () => {
+  addTaskBtnEl.classList.toggle("active");
+  addTaskFormEl.classList.toggle("active");
+  blackBackdropEl.classList.toggle("active");
+};
+
+addTaskBtnEl.addEventListener("click", toggleAddTaskForm);
+blackBackdropEl.addEventListener("click", toggleAddTaskForm);
